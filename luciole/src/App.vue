1e8aa43020a3c9119f1ed6c22c69f24865500373
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-03-29 12:19:14 +0200

    Integrate dark mode toggle.

--- luciole/src/App.vue
+++ luciole/src/App.vue
@@ -1,31 +1,35 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ToggleDarkMode from './components/ToggleDarkMode.vue'
 </script>
 
 <template>
 
-  <header>
+  <header class="text-black dark:text-white bg-white dark:bg-gray-800">
       <nav class="flex justify-between px-16 py-4">
         <div class="w-20 flex items-center">
           <RouterLink to="/">JAJI LOGO</RouterLink>
         </div>
         <ul class="flex justify-evenly w-60">
             <li class="mr-3">
-              <RouterLink to="/information-list" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4">Informations</RouterLink>
+              <RouterLink to="/information-list" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-black">Informations</RouterLink>
             </li>
             <li class="mr-3">
-              <a class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4" href="#">Santé</a>
+              <a class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-black" href="#">Santé</a>
             </li>
             <li class="mr-3">
-              <a class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4" href="#">Assurance</a>
+              <a class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-black" href="#">Assurance</a>
             </li>
             <li class="mr-3">
-              <RouterLink to="/profile" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4">Profil</RouterLink>
+              <RouterLink to="/profile" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-black">Profil</RouterLink>
             </li>
           <li class="mr-3">
-              <RouterLink to="/login" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4">Connexion</RouterLink>
+              <RouterLink to="/login" class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-black">Connexion</RouterLink>
           </li>
-          </ul>
+          <li class="mr-3">
+              <ToggleDarkMode />
+          </li>
+        </ul>
         <div class="w-20">&nbsp;</div>
       </nav>
   </header>
@@ -40,7 +44,6 @@ import { RouterLink, RouterView } from 'vue-router'
 <style scoped>
 
   header {
-    color: black;
     background-color: var(--color-background-soft);
   }
 
--- luciole/src/components/ToggleDarkMode.vue
+++ luciole/src/components/ToggleDarkMode.vue
@@ -6,27 +6,10 @@ const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <div class="flex justify-center items-center h-screen flex-col">
     <button
       @click="toggleDark()"
-      class="px-4 py-2 text-white bg-gray-600 dark:bg-purple-700"
+      class="inline-block border border-white rounded hover:border-gray-200 text-black hover:bg-white py-2 px-4 dark:text-white dark:hover:text-gray-800 whitespace-nowrap"
     >
       Dark Toggle
     </button>
-    <div class="mt-6">
-      <a
-        href="#"
-        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        <h5
-          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-        >
-          Vue 3 dark Mode With Tailwind CSS
-        </h5>
-        <p class="font-normal text-gray-700 dark:text-gray-400">
-          install & setup vue 3 dark mode in vue 3 using tailwind css 3
-        </p>
-      </a>
-    </div>
-  </div>
 </template>
