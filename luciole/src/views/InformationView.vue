b4cf6ac6047fca692ea29fc7df15998741918103
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-03-31 16:01:00 +0200

    Add theming for single news.

--- luciole/src/views/InformationView.vue
+++ luciole/src/views/InformationView.vue
@@ -1,11 +1,28 @@
 <template>
 
-  <div>
+  <div class="container mx-24">
     <div>
-      <h1 class="text-4xl font-bold">Post</h1>
-      <p>Title: {{title}}</p>
-      <p>Content: {{content}}</p>
-      <p>type: {{type}}</p>
+      <RouterLink to="/actualites" class="text-2xl font-bold text-blue-500">Retour</RouterLink>
+      <h1 class="text-4xl font-bold">{{title}}</h1>
+      <!-- Post Content -->
+      <div class="mt-8">
+        <div class="flex flex-col">
+          <div class="flex flex-col gap-2 max-w-7xl">
+            <p class="text-md text-gray-500">Type de contenu : {{type}}</p>
+            <div class="flex flex-col justify-center gap-2">
+              <p class="text-xl">{{content}}</p>
+            </div>
+            <div class="flex flex-col justify-center gap-2">
+            </div>
+            <div class="flex flex-col justify-center gap-2" v-if="medias.length > 0">
+              <p class="text-2xl font-bold">Medias</p>
+              <div class="flex flex-col items-center justify-center gap-2">
+                <img v-for="media in medias" :src="`http://localhost:3000/${media.path}`" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     <li v-for="media in medias">
         <img :src="`http://localhost:3000/${media.path}`" />
     </li>
